69dad1500efca8cc7d6f9c26e4d6044ce185ac8b
Rabit <rabit2245@gmail.com>
2021-11-18 20:52:49 +0100

    added timestamp, createdAt, orderBy functionality

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp } from 'firebase/app'
 import {
-  getFirestore,
-  collection,
-  onSnapshot,
-  addDoc,
-  deleteDoc,
-  doc,
-} from 'firebase/firestore';
+  getFirestore, collection, onSnapshot,
+  addDoc, deleteDoc, doc,
+  query, where,
+  orderBy, serverTimestamp,
+  getDoc,
+} from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyA2SSM0xzS3yxgll9d54WCKuiyvRHJExJU',
@@ -16,44 +15,64 @@ const firebaseConfig = {
   messagingSenderId: '86043521754',
   appId: '1:86043521754:web:fb59902fa9592e93ee2be3',
 };
-// init firebase app
-initializeApp(firebaseConfig);
 
-//init services
-const db = getFirestore();
+// init firebase
+initializeApp(firebaseConfig)
 
-//collection reference
-const colRef = collection(db, 'books');
+// init services
+const db = getFirestore()
 
-// real time collection data
-onSnapshot(colRef, (snapshot) => {
-  let books = [];
-  snapshot.docs.forEach((doc) => {
-    books.push({ ...doc.data(), id: doc.id });
-  });
-  console.log(books);
-});
+// collection ref
+const colRef = collection(db, 'books')
+
+// queries
+const q = query(colRef, where("author", "==", "patrick rothfuss"), orderBy('createdAt'))
+
+// realtime collection data
+onSnapshot(q, (snapshot) => {
+  let books = []
+  snapshot.docs.forEach(doc => {
+    books.push({ ...doc.data(), id: doc.id })
+  })
+  console.log(books)
+})
 
 // adding docs
-const addBookForm = document.querySelector('.add');
+const addBookForm = document.querySelector('.add')
 addBookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
   addDoc(colRef, {
     title: addBookForm.title.value,
     author: addBookForm.author.value,
-  }).then(() => {
-    addBookForm.reset();
-  });
-});
+    createdAt: serverTimestamp()
+  })
+  .then(() => {
+    addBookForm.reset()
+  })
+})
+
 // deleting docs
-const deleteBookForm = document.querySelector('.delete');
+const deleteBookForm = document.querySelector('.delete')
 deleteBookForm.addEventListener('submit', (e) => {
-  e.preventDefault();
+  e.preventDefault()
 
-  const docRef = doc(db, 'books', deleteBookForm.id.value);
+  const docRef = doc(db, 'books', deleteBookForm.id.value)
 
-  deleteDoc(docRef).then(() => {
-    deleteBookForm.reset();
-  });
-});
+  deleteDoc(docRef)
+    .then(() => {
+      deleteBookForm.reset()
+    })
+})
+
+// fetching a single document (& realtime)
+const docRef = doc(db, 'books', 'gGu4P9x0ZHK9SspA1d9j')
+
+// getDoc(docRef)
+//   .then(doc => {
+//     console.log(doc.data(), doc.id)
+//   })
+
+onSnapshot(docRef, (doc) => {
+  console.log(doc.data(), doc.id)
+})
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: 'development',
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
